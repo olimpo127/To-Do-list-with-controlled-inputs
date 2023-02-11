2af2423c6db4cf81c1a3997708365a805a375998
Felipe Ojeda <fojedai47@gmail.com>
2023-02-10 21:43:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
-function ToDoApp() {
-  const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState([]);
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-  const handleChange = (e) => {
-    setTask(e.target.value);
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos'));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    } else {
+      fetch('http://assets.breatheco.de/apis/fake/todos/user/johndoe')
+        .then(response => response.json())
+        .then(data => setTodos(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    if (inputValue) {
+      const newTodo = {
+        label: inputValue,
+        done: false,
+        id: todos.length + 1
+      };
+      setTodos([...todos, newTodo]);
+      setInputValue('');
+    }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (task !== "") {
-    setTasks([...tasks, task]);
-    setTask("");
-    }
-    };
-
-  const handleDelete = (index) => {
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
-    setTasks(newTasks);
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
   };
 
   return (
-    <div style={{ textAlign: "center", margin: "100px", marginLeft: "150px", marginRight: "150px"}}>
-      <h1>To Do App</h1>
-      <div style={{ textAlign: "center", marginLeft: "200px", marginRight: "200px", borderStyle: "solid", borderColor: "grey" }}>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="What needs to be done?"
-          value={task}
-          onChange={handleChange}
-          style={{display: "flex", justifyContent: "space-between", padding: "10px", width: "575px", border: "none", borderBottom: "solid", borderColor: "grey"}}
-        />
-      </form>
-      <div>
-        {tasks.length > 0 ? (
-          tasks.map((t, index) => (
-            <div key={index} style={{ display: "flex", justifyContent: "space-between", borderBottom: "1px solid black", padding: "10px" }}>
-              <p>{t}</p>
-              <button style={{padding: "5px", border: "none"}} onClick={() => handleDelete(index)}>X</button>
-            </div>
-          ))
-        ) : (
-          <p style={{border: "solid", borderColor: "grey", background: "yellow"}}><strong>No tasks, add a task</strong></p>
-        )}
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ width: '50%', border: '1px solid gray', borderRadius: '5px', padding: '20px' }}>
+        <h1>To-Do App</h1>
+        <form onSubmit={addTodo}>
+          <input type="text" placeholder="Add a to-do item" value={inputValue} onChange={event => setInputValue(event.target.value)} />
+          <button type="submit">Add</button>
+        </form>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          {todos.map(todo => (
+            <li key={todo.id} style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', border: '1px solid gray', borderRadius: '5px' }}>
+              {todo.label}
+              <button style={{ backgroundColor: 'transparent', border: 'none', cursor: 'pointer' }} onClick={() => deleteTodo(todo.id)}>&times;</button>
+            </li>
+          ))}
+        </ul>
       </div>
-      <p style={{display: "flex", justifyContent:"flex-start", fontSize:" 10px", marginLeft:"3px"}}>{tasks.length} item{tasks.length === 1 ? "" : "s"} left</p>
-    </div>
     </div>
   );
-}
+};
+
+export default App;
 
